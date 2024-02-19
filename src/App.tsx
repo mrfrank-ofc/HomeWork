b99ca04effcae1928f9f39ed19e5346b61c9348a
joaosmendess <joaosilva0721@gmail.com>
2024-02-19 18:54:50 -0300

    feat/Organize folders

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <>
-     <h1>Hello word</h1>
+     <h1 className="font-heading text-3xl mb-4 font-bold">Hello word</h1>
+     <p className="font-body text-base">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam sunt id corporis ullam voluptatem. Doloremque corrupti perferendis eveniet voluptatem ex? Voluptate obcaecati rerum fuga. Expedita error magni a sit facere!</p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   darkMode: 'class', // Usando 'class' para controle manual do tema escuro
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Mulish', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
