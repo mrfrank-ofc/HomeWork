7413db9d0bb70f6ba8f67aae86d603a9f59ec439
joaosmendess <joaosilva0721@gmail.com>
2024-02-19 18:36:16 -0300

    feat/ Install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Hello word</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // Usando 'class' para controle manual do tema escuro
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
