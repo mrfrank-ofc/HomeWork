29def74afa3716d6ebc7c3b77d20942b6ba83f21
joaosmendess <joaosilva0721@gmail.com>
2024-02-23 11:49:06 -0300

    feat/Implements section for about me

--- src/components/layout/Nav/index.tsx
+++ src/components/layout/Nav/index.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React  from 'react'
+import { useMenuContext } from '../../../context/MenuContext'
 
 const Nav: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const { isMenuOpen, toggleMenu } = useMenuContext();
 
-  const closeMenu = () => {
-    setIsMenuOpen(false)
-  }
+
+ 
 
   return (
     <>
-      <button onClick={() => setIsMenuOpen(true)} className='text-xl md:hidden'>
+      <button onClick={ toggleMenu} className='text-xl md:hidden ' aria-expanded={isMenuOpen}>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
@@ -29,7 +29,7 @@ const Nav: React.FC = () => {
         className={`fixed inset-0 md:static bg-light dark:bg-dark z-50 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full md:translate-x-0'} transition-transform duration-300 ease-in-out`}
       >
         <div className='flex justify-end p-4 md:hidden'>
-          <button onClick={closeMenu}>
+          <button onClick={toggleMenu}>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -46,24 +46,24 @@ const Nav: React.FC = () => {
             </svg>
           </button>
         </div>
-        <nav className='flex flex-col gap-y-10 md:flex-row items-center justify-center h-full md:h-auto font-heading '>
+        <nav className='flex flex-col gap-y-10 md:flex-row items-center justify-center h-full md:h-auto font-heading  bg-white dark:bg-dark '>
           <a
             href='#home'
-            onClick={closeMenu}
-            className='block py-2 px-4 text-textLight dark:text-textDark md:py-0'
+            onClick={toggleMenu}
+            className='block py-2 px-6 md:py-0 t'
           >
             Home
           </a>
           <a
             href='#about'
-            onClick={closeMenu}
+            onClick={toggleMenu}
             className='block py-2 px-4 md:py-0'
           >
             About
           </a>
           <a
             href='#projects'
-            onClick={closeMenu}
+            onClick={toggleMenu}
             className='block py-2 px-4 md:
 '
           >
@@ -71,13 +71,14 @@ const Nav: React.FC = () => {
           </a>
           <a
             href='#contact'
-            onClick={closeMenu}
+            onClick={toggleMenu}
             className='block py-2 px-4 md:py-0'
           >
             Contact
           </a>
         </nav>
       </div>
+      
     </>
   )
 }
