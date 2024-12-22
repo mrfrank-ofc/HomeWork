d67b541a0d0738942beefaafcf2f22d3cf3c0db8
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-22 12:40:45 +0200

    Update index.tsx

--- src/components/sections/Contact/index.tsx
+++ src/components/sections/Contact/index.tsx
@@ -39,6 +39,17 @@ const Contact: React.FC = () => {
                 </div>
               </div>
 
+              <div className="flex flex-col items-center md:items-start gap-2">
+                <span className="gradient-text font-body text-lg">My WhatsApp</span>
+                <div className="flex items-center gap-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="tabler-icon tabler-icon-mail">
+                    <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
+                    <path d="M3 7l9 6l9 -6"></path>
+                  </svg>
+                  <a href="wa.me/263719647303/?text=Hi+Darrell+I+Was+Reading+Your+Portfolii+And+...." className="underline ">WhatsApp Contact </a>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
