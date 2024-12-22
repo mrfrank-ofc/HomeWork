5ff0808effd59fb7bc7e2972e1c726f8f03ecd87
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-22 12:45:56 +0200

    Update index.tsx

--- src/components/sections/Contact/index.tsx
+++ src/components/sections/Contact/index.tsx
@@ -46,7 +46,7 @@ const Contact: React.FC = () => {
                     <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
                     <path d="M3 7l9 6l9 -6"></path>
                   </svg>
-                  <a href="wa.me/263719647303/?text=Hi+Darrell+I+Was+Reading+Your+Portfolii+And+...." className="underline ">WhatsApp Contact </a>
+                  <a href="https://wa.me/263719647303/?text=Hi+Darrell+I+Was+Reading+Your+Portfolii+And+...." className="underline ">WhatsApp Contact </a>
                 </div>
               </div>
 
